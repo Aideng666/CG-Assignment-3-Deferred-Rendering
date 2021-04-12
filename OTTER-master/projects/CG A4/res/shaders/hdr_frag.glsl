5168e15fe9a095598a9e461fd82ad00368aca6a5
Aiden <aiden.gimpel@ontariotechu.net>
2021-04-11 21:11:39 -0400

    Blah

--- OTTER-master/projects/CG A4/res/shaders/hdr_frag.glsl
+++ OTTER-master/projects/CG A4/res/shaders/hdr_frag.glsl
@@ -5,24 +5,17 @@ layout(location = 0) in vec2 inUV;
 out vec4 frag_color;
   
 uniform sampler2D hdrBuffer;
-uniform bool u_hdr;
 uniform float u_exposure;
 
 void main()
 {             
     const float gamma = 2.2;
     vec3 hdrColor = texture(hdrBuffer, inUV).rgb;
-    //if(u_hdr)
-   // {
+
         // exposure
         vec3 result = vec3(1.0) - exp(-hdrColor * u_exposure);
         // also gamma correct while we're at it       
         result = pow(result, vec3(1.0 / gamma));
         frag_color = vec4(result, 1.0);
-   // }
-   // else
-   // {
-   //     vec3 result = pow(hdrColor, vec3(1.0 / gamma));
-   //     frag_color = vec4(result, 1.0);
-   // }
+
 }
